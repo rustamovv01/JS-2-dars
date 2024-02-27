a56b9d21d2d747800e52287505a1995c9e059cd8
Mukhammadaziz <rustamov977075073@gmail.com>
2024-02-27 15:23:20 +0500

    2-dars

---
+++ main.js
@@ -0,0 +1,53 @@
+// // // string 
+
+// // let ism = 'Muhammadaziz';
+
+// // // number 
+
+// // let son = 25;
+
+// // // boolean 
+
+// // let a = true;
+// // let b = false;
+
+// // // null 
+
+// // const data = null;
+
+// // // undefined 
+
+// // const data2 = undefined;
+
+// // // symbol 
+
+// // let d = Symbol();
+
+// // // bigInt 
+
+// // let raqam = 11111111234543256578564321456786654323456;
+
+// // // object
+
+// // let obj = {
+// //     name: 'Muhammadaziz',
+// //     surName: 'Rustamov',
+// //     age: 14,
+// // }
+
+// let son1 = 12;
+// let son2 = '10';
+// let son3 = 23;
+
+// console.log(son1 + son2 + son3);
+
+// let name = prompt('qayerdansiz aka ');
+// alert(`${name}  ashi yerda turesizme aka`);
+
+let ism = prompt('String type da malumot kiriting');
+let raqam = prompt('Number type da malumot kiriting');
+let bol = prompt('Boolean type da malumot kiriting');
+
+alert(`siz kiritgan ${ism} String type ga tegishli \n 
+siz kiritgan raqam ${raqam} Number type ga tegishli \n 
+siz kiritgan boolean ${bol} Boolean type ga tegishli`);
